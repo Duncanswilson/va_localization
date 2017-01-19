7fbfc97546b24a1e5c2cd8db292bb4e587222f7b
Richard Kelley <richard.kelley@gmail.com>
2017-01-18 21:54:14 -0800

    Fixed problems with data loader.

--- util/reno_driving.py
+++ util/reno_driving.py
@@ -19,7 +19,7 @@ def convert_gps(coords, ref_pt):
     lat = coords[:, 0]
     lon = coords[:, 1]
     alt = coords[:, 2]
-    
+
     local_ned = navpy.lla2ned(lat, lon, alt, ref_pt[0], ref_pt[1], ref_pt[2])
 
 def normalize_gps(coords, ref_pt):
@@ -35,19 +35,19 @@ def normalize_gps(coords, ref_pt):
     return (x - means)/stds
 
 class RenoDriving(object):
-    
+
     def __init__(self, image_path, coord_path):
         self.images = {}
         self.coords = {}
         self.NUM_COORDS = 3
 
         self.create_dataset(image_path, coord_path)
-        
+
         if set(self.images.keys()) != set(self.coords.keys()):
             print("Keys and coordinates don't match!!")
 
         self.processed = []
-        
+
         self.epochs = 0
 
     def create_dataset(self, image_path, coord_path):
@@ -57,8 +57,9 @@ class RenoDriving(object):
             for f in files:
                 match = re.search("\d*", f)
                 num = int(match.group(0))
+                #print(num)
                 img = cv2.imread(os.path.join(subdir,f))
-                self.images[num] = 1.0 - img #cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+                self.images[num] = 1 - cv2.resize(img[:,:,[2,1,0]], (0,0), fx=0.5, fy=0.5)
                 idx += 1
 
         # get coords
@@ -66,7 +67,7 @@ class RenoDriving(object):
             for f in files:
                 match = re.search("\d*", f)
                 num = int(match.group(0))
-                
+
                 f = open(os.path.join(subdir, f), 'r')
                 for line in f:
                     parts = line.split()
@@ -78,7 +79,7 @@ class RenoDriving(object):
         # get indices
         keys = self.images.keys()
         candidates = list(set(keys) - set(self.processed))
-        
+        print(len(self.images))
         rows = self.images[candidates[0]].shape[0]
         cols = self.images[candidates[0]].shape[1]
         chans = self.images[candidates[0]].shape[2]
@@ -86,6 +87,8 @@ class RenoDriving(object):
         # handle case where we have used all the indices but have more
         # to do
         if len(candidates) < batch_size:
+            print len(candidates), batch_size
+
             # first add all the candidates
             indices = candidates
 
@@ -108,7 +111,7 @@ class RenoDriving(object):
         image_array = np.zeros((batch_size, rows, cols, chans))
         idx = 0
         for i in indices:
-            image_array[idx] = cv2.cvtColor(self.images[i], cv2.COLOR_BGR2RGB)
+            image_array[idx] = self.images[i]
             idx += 1
 
         # pack coords into array
